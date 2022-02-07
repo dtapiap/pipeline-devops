02e0540dac42ffaf59522f7b7eb6431e0410807f
Daniel Tapia <danieltapia@MacBook-Pro-de-Daniel.local>
2022-02-07 00:21:58 -0300

    agregando que ejecuta maven o gradle

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -15,7 +15,7 @@ pipeline {
     stages {
         stage("Pipeline"){
             steps {
-                env.varTOOLS
+                
                 figlet  "${varTOOLS}"
                 script{
                   switch(params.compileTool)
